f222189a906d3e9d46efeb42eec7c069b60d5ff6
Spencer Jones <spencerjones@fnbo.com>
2023-11-16 20:54:50 -0600

    Add image and break into components

--- App.js
+++ App.js
@@ -1,64 +1,19 @@
-import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import CurrentWeather from "./src/components/CurrentWeather"; 
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text>Current Weather</Text>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8</Text>
-          <Text style={styles.highLow}>Low: 2</Text>
-        </View>
-        <View style={styles.bodyWrapper}>
-          <Text style={styles.description}>It's sunny</Text>
-          <Text style={styles.message}>It's perfect t-shirt weather</Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+    <View style={styles.container}>
+    <CurrentWeather />
+    </View>
+  )
 }
-
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    alignContent: 'center',
-  },
   container: {
-    backgroundColor: 'lightgreen',
     flex: 1,
-    alignItems: 'center',
   },
-  temp: {
-    color: 'black',
-    fontSize: 48
-  },
-  feels: {
-    color: 'black',
-    fontSize: 30
-  },
-  highLow: {
-    color: 'black',
-    fontSize: 20
-  },
-  highLowWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '50%',
-  },
-  bodyWrapper: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
-  },
-  description: {
-    fontSize: 38,
-  },
-  message: {
-    fontSize: 30,
-  }
 });
 
 export default App;
---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import { Feather as Icon } from '@expo/vector-icons';
+
+const CurrentWeather = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Icon name="sun" size={100} color="black" />
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 2</Text>
+        </View>
+        <View style={styles.bodyWrapper}>
+          <Text style={styles.description}>It's sunny</Text>
+          <Text style={styles.message}>It's perfect t-shirt weather</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignContent: 'center',
+  },
+  container: {
+    backgroundColor: 'lightgreen',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 40,
+  },
+  description: {
+    fontSize: 38,
+  },
+  message: {
+    fontSize: 30,
+  }
+});
+
+export default CurrentWeather;
