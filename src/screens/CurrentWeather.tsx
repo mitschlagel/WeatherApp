4dbcdde69b89562b3bc446fe3afec670d9469496
Spencer Jones <sjonesdrums@gmail.com>
2023-11-18 23:31:03 -0600

    Add navigation

--- src/screens/CurrentWeather.tsx
+++ src/screens/CurrentWeather.tsx
@@ -2,22 +2,42 @@ import React from "react";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
 import { Feather as Icon } from "@expo/vector-icons";
+import RowText from "../components/RowText";
 
 const CurrentWeather: React.FC = () => {
+
+  const {
+    wrapper,
+    container,
+    temp,
+    feels,
+    highLowWrapper,
+    highLow,
+    bodyWrapper,
+    description,
+    message
+  } = styles
+  
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
+    <SafeAreaView style={wrapper}>
+      <View style={container}>
         <Icon name="sun" size={100} color="black" />
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8</Text>
-          <Text style={styles.highLow}>Low: 2</Text>
-        </View>
-        <View style={styles.bodyWrapper}>
-          <Text style={styles.description}>It's sunny</Text>
-          <Text style={styles.message}>It's perfect t-shirt weather</Text>
-        </View>
+        <Text style={temp}>6</Text>
+        <Text style={feels}>Feels like 5</Text>
+        <RowText 
+          messageOne={'High: 8'} 
+          messageTwo={'Low: 6'} 
+          containerStyles={highLowWrapper}
+          messageOneStyles={highLow}
+          messageTwoStyles={highLow}
+        />
+         <RowText 
+          messageOne={"It's sunny"} 
+          messageTwo={"It's perfect t-shirt weather"} 
+          containerStyles={bodyWrapper}
+          messageOneStyles={description}
+          messageTwoStyles={message}
+        />
       </View>
     </SafeAreaView>
   );
@@ -26,13 +46,13 @@ const CurrentWeather: React.FC = () => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    alignContent: "center",
+    alignContent: "center"
   },
   container: {
     backgroundColor: "lightblue",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   temp: {
     color: "black",
@@ -48,7 +68,7 @@ const styles = StyleSheet.create({
   },
   highLowWrapper: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    padding: 20
   },
   bodyWrapper: {
     alignItems: "center",
