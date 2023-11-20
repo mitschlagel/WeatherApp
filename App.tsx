f38342723e39eb35269ab2ae11afe1c962ddbd92
Spencer Jones <sjonesdrums@gmail.com>
2023-11-19 21:18:16 -0600

    Add tab navigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 
+const Tab = createBottomTabNavigator()
+
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CurrentWeather}/>
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather}/>
+        <Tab.Screen name={'Location'} component={City}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
 
